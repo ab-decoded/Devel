53d96bc7a2076c1a3bfc250c505dd722e3277174
Abhinav Bansal <abhinavbansal0@gmail.com>
2016-10-09 11:59:27 +0530

    Fix for static files

--- devel_main/settings.py
+++ devel_main/settings.py
@@ -155,8 +155,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
